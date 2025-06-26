48cf10931bdd6971123913ed595d8a470e283ad3
Umut Piynar <12987255+uruskan@users.noreply.github.com>
2025-06-26 19:39:02 +0300

    Remove all TypeScript files and references

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,jsx,mdx}',
+    './components/**/*.{js,jsx,mdx}',
+    './app/**/*.{js,jsx,mdx}',
   ],
   theme: {
     extend: {
