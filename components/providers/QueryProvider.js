5593650374379e82541215f50105a60a3fca084b
Umut Piynar <12987255+uruskan@users.noreply.github.com>
2025-07-01 15:00:37 +0300

    Updated package-lock.json

--- components/providers/QueryProvider.js
+++ components/providers/QueryProvider.js
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
 import { queryClient } from '@/lib/react-query/client';
 
 export function QueryProvider({ children }) {
